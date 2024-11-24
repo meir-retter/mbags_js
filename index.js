4e78a96fa0e2baec88c7834a8329fc12663d8624
meir-retter <mretter20@gmail.com>
2024-11-24 15:18:48 -0500

    another

--- index.js
+++ index.js
@@ -1,63 +1,81 @@
+const numGrids = 2;
+const numRows = 3;
+const numCols = 3;
+
 class GameGrid {
-  constructor(num_rows, num_cols, goal_r, goal_c, mbag_r, mbag_c) {
-    this.num_rows = num_rows
-    this.num_cols = num_cols
+  constructor(grid_index, goal_r, goal_c, mbag_r, mbag_c) {
+    this.grid_index = grid_index;
+    this.num_rows = numRows;
+    this.num_cols = numCols;
     this.goal_r = goal_r;
     this.goal_c = goal_c;
     this.mbag_r = mbag_r;
     this.mbag_c = mbag_c;
+  }
 
+  get_grid_element() {
+    return document.getElementById("grid" + this.grid_index);
+  }
+
+  set_up() {
+    let grid_element = this.get_grid_element();
+    grid_element.style.setProperty('--grid-rows', numRows);
+    grid_element.style.setProperty('--grid-cols', numCols);
+    for (let r = 0; r < numRows; r++) {
+      for (let c = 0; c < numCols; c++) {
+        let cell_num = 3*r + c
+        let cell = document.createElement("div");
+        cell.setAttribute("id", "grid" + this.grid_index + "cell" + cell_num)
+        cell.innerText = ".";
+        cell.className = "grid-item";
+        grid_element.appendChild(cell);
+      }
+    }
+  }
+
+  refresh() {
+    for (let r = 0; r < numRows; r++) {
+      for (let c = 0; c < numCols; c++) {
+        let cell_num = 3*r + c
+        let cell = document.getElementById("grid" + this.grid_index + "cell" + cell_num);
+        let symbol;
+        if (r === this.mbag_r && c === this.mbag_c) {
+          symbol = "M"
+        } else if (r === this.goal_r && c === this.goal_c) {
+          symbol = "G"
+        } else {
+          symbol = "."
+        }
+        cell.innerText = symbol;
+      }
+    };
   }
 }
 
-const grid1 = document.getElementById("grid1");
-function makeRows(rows, cols) {
-  grid1.style.setProperty('--grid-rows', rows);
-  grid1.style.setProperty('--grid-cols', cols);
-  for (r = 0; r < rows; r++) {
-    for (c = 0; c < cols; c++) {
-      cell_num = 3*r + c
-      let cell = document.createElement("div");
-      cell.setAttribute("id", "cell" + cell_num)
-      cell.innerText = ".";
-      grid1.appendChild(cell).className = "grid-item";
-    }
-  };
-};
 
-function populateRows(rows, cols, goal_r, goal_c, mbag_r, mbag_c) {
-  for (r = 0; r < rows; r++) {
-    for (c = 0; c < cols; c++) {
-      cell_num = 3*r + c
-      let cell = document.getElementById("cell" + cell_num);
-      let symbol;
-      if (r === mbag_r && c === mbag_c) {
-        symbol = "M" 
-      } else if (r === goal_r && c === goal_c) {
-        symbol = "G"
-      } else {
-        symbol = "."
-      }
-      cell.innerText = symbol;
-    }
-  };
-};
 
-let gg = new GameGrid(3, 3, 0,0,0,1);
-makeRows(gg.num_rows, gg.num_cols);
-populateRows(gg.num_rows, gg.num_cols, gg.goal_r, gg.goal_c, gg.mbag_r, gg.mbag_c);
+let grids = [];
+for (let i = 0; i < numGrids; i++) {
+  grid = new GameGrid(i, 0, 0,0,1);
+  grid.set_up()
+  grid.refresh()
+  grids.push(grid)
+}
 
 document.addEventListener('keydown', function(event) {
-  if (event.key === 'ArrowUp') {
-    gg.mbag_r -= 1
-  } else if (event.key === 'ArrowDown') {
-    gg.mbag_r += 1
-  } else if (event.key === 'ArrowLeft') {
-    gg.mbag_c -= 1
-  } else if (event.key === 'ArrowRight') {
-    gg.mbag_c += 1
-  }
-  populateRows(gg.num_rows, gg.num_cols, gg.goal_r, gg.goal_c, gg.mbag_r, gg.mbag_c);
+  grids.forEach((gg, i) => {
+    if (event.key === 'ArrowUp') {
+      gg.mbag_r -= 1
+    } else if (event.key === 'ArrowDown') {
+      gg.mbag_r += 1
+    } else if (event.key === 'ArrowLeft') {
+      gg.mbag_c -= 1
+    } else if (event.key === 'ArrowRight') {
+      gg.mbag_c += 1
+    }
+    gg.refresh()
+  })
+
 });
 
 
